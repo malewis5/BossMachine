aa80805646e2308c5022d271755ef889f0f5bb9b
Matthew Lewis <malewis5@crimson.ua.edu>
2020-10-15 15:23:34 -0400

    implented routes for minions and ideas.

---
+++ server/minions.js
@@ -0,0 +1,51 @@
+const { NamedModulesPlugin } = require("webpack");
+
+const minionsRouter = require("expresss").Router();
+
+module.exports = minionsRouter;
+
+const {
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabaseById,
+} = require("./db");
+
+minionsRouter.param("minionId", (req, res, next, id) => {
+  const minion = getFromDatabaseById("minions", id);
+  if (minion) {
+    req.minion = minion;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
+minionsRouter.get("/", (req, res, next) => {
+  res.send(getAllFromDatabase("minions"));
+});
+
+minionsRouter.post("/", (req, res, next) => {
+  const newMinion = addToDatabase("minions", req.body);
+  res.status(201).send(newMinion);
+});
+
+minionsRouter.get("/:minionId", (req, res, next) => {
+  res.send(req.minion);
+});
+
+minionsRouter.put("/:minionId", (req, res, next) => {
+  const updatedMinion = updateInstanceInDatabase("minions", req.body);
+  res.send(updatedMinion);
+});
+
+minionsRouter.delete("/:minionId", (req, res, next) => {
+  const deletedMinion = deleteFromDatabaseById("minions", req.minion);
+  if (deletedMinion) {
+    res.status(204);
+  } else {
+    res.status(500);
+  }
+  res.send();
+});
